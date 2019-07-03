6bb45bb0cc7dac347e4a4386dad34a0a95e9d3ab
earthtowalt <jackwalton00@aol.com>
2019-07-02 22:06:44 -0400

    finished movement and aim and shoot for gunner.

--- learning.py
+++ learning.py
@@ -35,14 +35,14 @@ class Gunner(object):
 		self.original_gunner = GUNNER.subsurface((0,0,150,150))
 		self.original_feet = GUNNER.subsurface((300,0,150,150))
 		
-		### should be replaced with call to position()
+		### fixme: label something
 		self.gunner = self.original_gunner.copy()
 		self.feet = self.original_feet.copy()
 		self.gunner_rect = self.gunner.get_rect(center=self.location)
 		self.feet_rect = self.feet.get_rect(center=self.location)
-		###
 		
-	def position(self):
+		
+	def set_position(self):
 		# use:
 		# self.angle
 		# self.location
@@ -52,61 +52,34 @@ class Gunner(object):
 		self.gunner = pygame.transform.rotate(self.original_gunner, self.gunner_angle)
 		self.feet = pygame.transform.rotate(self.original_feet, self.feet_angle)
 		# rotate sprites to correct orientation.
-		self.gunner = pygame.transform.rotate(self.gunner, self.gunner_angle)
-		self.feet = pygame.transform.rotate(self.feet, self.feet_angle)
+		#self.gunner = pygame.transform.rotate(self.gunner, self.gunner_angle)
+		#self.feet = pygame.transform.rotate(self.feet, self.feet_angle)
 		# position the sprites correctly
 		self.gunner_rect = self.gunner.get_rect(center=self.location)
 		self.feet_rect = self.feet.get_rect(center=self.location)
 		
 		
-	def handle_input(self, deadzone=0.1):
-		'''
-		get the current angle of the passed sticks 
-		and set the according sprites.
-		'''
-	
-		leftx,lefty = self.controller.get_axis(0), self.controller.get_axis(1)
+	def aim(self, deadzone=0.1):
+		'''calculate gunner_angle'''
 		rightx,righty = self.controller.get_axis(3), -self.controller.get_axis(4)
-		
-		if abs(leftx) > deadzone or abs(lefty) > deadzone:
-			# set the angle
-			if leftx == 0.0: leftx += 0.0001
-			self.feet_angle = 225.0 -math.degrees(math.atan2(float(lefty), float(leftx)))
-			
-			#fixme self.feet = pygame.transform.rotate(self.original_feet, self.feet_angle)
-			#fixme self.feet_rect = self.feet.get_rect(center=self.feet_rect.center)
-			# move the feet and gunner
-			self.location[0] += self.speed * leftx
-			self.location[1] += self.speed * lefty 
-			'''
-			if abs(leftx) > deadzone:
-				x += speed * axis0 # move horiz
-			if abs(lefty) > deadzone:
-				y += speed * axis1 # move vert
-				self.gunner.transform.()
-				self.feet.transform()
-			'''
-				
-			'''
-			movement control:
-			# control movement
-				if abs(axis0) > deadzone:
-					x += speed * axis0
-				
-				if abs(axis1) > deadzone:
-					y += speed * axis1
-			'''
-				
-		# change the logic to just set the self.angle and self.x and self.y
 		if abs(rightx) > deadzone or abs(righty) > deadzone:
 			if rightx == 0.0: rightx += 0.0001
 			self.gunner_angle = 45.0 -math.degrees(math.atan2(float(righty), float(rightx)))
 			# transform and locate..
 			# fixme self.gunner = pygame.transform.rotate(self.original_gunner, self.gunner_angle)
 			# fixme self.gunner_rect = self.gunner.get_rect(center=self.gunner_rect.center)
-		self.position()
+		
 			
-	
+	def move(self, deadzone=0.1):
+		'''caclulate location and feet_angle'''
+		leftx,lefty = self.controller.get_axis(0), self.controller.get_axis(1)
+		if abs(leftx) > deadzone or abs(lefty) > deadzone:
+			# set the angle
+			if leftx == 0.0: leftx += 0.0001
+			self.feet_angle = 225.0 -math.degrees(math.atan2(float(lefty), float(leftx)))
+			# mooove
+			self.location[0] += self.speed * leftx
+			self.location[1] += self.speed * lefty 
 		
 		
 	def get_event(self, event, objects):
@@ -116,10 +89,16 @@ class Gunner(object):
 				objects.add(Bullet(self.gunner_rect.center, self.gunner_angle))
 		if event.type == pygame.JOYAXISMOTION:
 			if event.joy == self.id:
-				self.handle_input()
+				self.aim()
+		
+		
 	
 	def draw(self, surface):
 		'''draw gunner and feet to the target surface'''
+		# always move even if no event. 
+		self.move()
+		# then set the position
+		self.set_position()
 		surface.blit(self.feet, self.feet_rect)
 		surface.blit(self.gunner, self.gunner_rect)
 
@@ -178,6 +157,7 @@ class Control(object):
 	def update(self):
 		''' update all bullets'''
 		self.bullets.update(self.screen_rect)
+		# handle the movement. of left stick.
 		
 	def draw(self):
 		'''draw all elements to the display surface'''
